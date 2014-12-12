631417776f64b93b2b2ca5ca402bce2a9c1f01c4
Bobby Porwal <nporwal@msn.com>
2014-12-12 03:03:02 -0500

    Finished initial parser

--- parser.py
+++ parser.py
@@ -1,29 +1,56 @@
 import csv
 
-class Battle:
-    def __init__(self, data, num):
-        self.data = data
-        self.n = num
 
-def parse_battles(filename):
-    data = []
-    with open(filename) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            data.append(row)
-    return (data[0].keys(), data)
+class BattleData:
+    # Provide filenames in order 'battles', 'weather', 'durations', 'belligerents', 'dyads', 'terrain', 'widths'
+    def __init__(self, filenames):
+        self.n = 0
+        self.battles = []
+        battles, filenames = filenames[0], filenames[1:]
+        with open(battles) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                self.battles.append(row)
+                self.n += 1
+        self.weather = [None] * self.n
+        self.duration = [None] * self.n
+        self.belligerents = d = [[] for _ in xrange(self.n)]
+        self.terrain = [None] * self.n
+        self.dyad = [None] * self.n
+        self.width = [None] * self.n
+        self.__keys = {
+            'weather': self.weather,
+            'durations': self.duration,
+            'belligerents': self.belligerents,
+            'dyads': self.dyad,
+            'terrain': self.terrain,
+            'widths': self.width
+        }
+        if len(filenames) != len(self.__keys):
+            raise Exception("Not enough filenames provided, need to provide {} filenames".format(len(self.__keys)))
 
-def parse_and_add(filename, key, data):
-    with open(filename) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            i = row["isqno"]
-            del row["isqno"]
-            data[i][key] = row
+        keys = ['weather', 'durations', 'belligerents', 'dyads', 'terrain', 'widths']
+        for i in range(len(self.__keys)):
+            self.__parse_and_add(filenames[i], keys[i])
+
+    def __parse_and_add(self, filename, key):
+        data = self.__keys.get(key)
+        with open(filename) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                if key == 'belligerents':
+                    data[int(int(row["isqno"])-1)].append(row)
+                else:
+                    data[int(row["isqno"])-1] = row
 
 
 if __name__ == "__main__":
-    battles = parse_battles("data/battles.csv")
-    parse_and_add("data/weather.csv", )
-    for battle in battles[1]:
-        print battle['isqno']
+    battles = BattleData(["data/battles.csv", "data/weather.csv", "data/battle_durations.csv", "data/belligerents.csv",
+        "data/battle_dyads.csv", "data/terrain.csv", "data/front_widths.csv"])
+    print battles.battles[0]
+    print battles.belligerents[0]
+    print battles.weather[0]
+    print battles.duration[0]
+    print battles.terrain[0]
+    print battles.dyad[0]
+    print battles.width[0]
