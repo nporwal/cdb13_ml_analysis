40a67526dcd896ae116d28b8406b1b8ef47fb353
Bobby Porwal <nporwal@msn.com>
2014-12-13 23:37:27 -0500

    Parser flattens weather, terrain, battles.csv into a single array of battles

--- parser.py
+++ parser.py
@@ -1,8 +1,34 @@
 import csv
 
 
+def flatten(data):
+    new_data = []
+    for row in data:
+        if len(row) > 1:
+            d = {}
+            for k in row[0].keys():
+                s = ""
+                for col in row:
+                    if not col[k] in s:
+                        s += col[k]
+                d[k] = s
+            new_data.append(d)
+        else:
+            new_data.append(row[0])
+    return new_data
+
+
+def parse_and_add(filename, n):
+    data = [[] for _ in xrange(n)]
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            data[int(int(row["isqno"])-1)].append(row)
+    return flatten(data)
+
+
 class BattleData:
-    # Provide filenames in order 'battles', 'weather', 'durations', 'belligerents', 'dyads', 'terrain', 'widths'
+    # Provide filenames in order 'battles', 'weather', 'terrain'
     def __init__(self, filenames):
         self.n = 0
         self.battles = []
@@ -10,45 +36,26 @@ class BattleData:
         with open(battles) as f:
             reader = csv.DictReader(f)
             for row in reader:
+                if row["wina"] == "":
+                    row["wina"] = -1
                 self.battles.append(row)
                 self.n += 1
         # Index of the arrays correspond to isqno-1
-        self.weather = [None] * self.n
-        self.duration = [None] * self.n
-        self.belligerents = [[] for _ in xrange(self.n)]
-        self.terrain = [None] * self.n
-        self.width = [None] * self.n
-        self.__keys = {
-            'weather': self.weather,
-            'durations': self.duration,
-            'belligerents': self.belligerents,
-            'terrain': self.terrain,
-            'widths': self.width
-        }
-        if len(filenames) != len(self.__keys):
-            raise Exception("Not enough filenames provided, need to provide {} filenames".format(len(self.__keys)))
 
-        keys = ['weather', 'durations', 'belligerents', 'terrain', 'widths']
-        for i in range(len(self.__keys)):
-            self.__parse_and_add(filenames[i], keys[i])
+        weather = parse_and_add(filenames[0], self.n)
+        terrain = parse_and_add(filenames[1], self.n)
+        self.__combine(weather)
+        self.__combine(terrain)
 
-    def __parse_and_add(self, filename, key):
-        data = self.__keys.get(key)
-        with open(filename) as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                if key == 'belligerents':
-                    data[int(int(row["isqno"])-1)].append(row)
-                else:
-                    data[int(row["isqno"])-1] = row
+    def __combine(self, data):
+        if len(data) != len(self.battles):
+            raise Exception("arg data incorrect size")
+        for i, b in enumerate(self.battles):
+            row = data[i]
+            for k, v in row.items():
+                b[k] = v
 
 
 if __name__ == "__main__":
-    battles = BattleData(["data/battles.csv", "data/weather.csv", "data/battle_durations.csv", "data/belligerents.csv",
-        "data/terrain.csv", "data/front_widths.csv"])
+    battles = BattleData(["data/battles.csv", "data/weather.csv", "data/terrain.csv"])
     print battles.battles[0]
-    print battles.belligerents[0]
-    print battles.duration[0]
-    print battles.terrain[0]
-    print battles.weather[0]
-    print battles.width[0]
